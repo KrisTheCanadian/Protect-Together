66eaa16c25facfa2d5d29243968c25a8f27d792c
Kris <kristhecanadian101@gmail.com>
2022-02-07 23:44:11 -0500

    persistent login firebase

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import { auth } from '../../config/firebase_config';
 import NotFound from '../../pages/NotFound';
@@ -7,8 +7,25 @@ interface IAuthRouteProps {
   component: JSX.Element
 }
 
+function onAuthStateChange(callback : any) {
+  return auth.onAuthStateChanged((user: any) => {
+    if (user) {
+      callback({ loggedIn: true });
+    } else {
+      callback({ loggedIn: false });
+    }
+  });
+}
+
 const AuthRequired: React.FC<IAuthRouteProps> = ({ component }) => {
-  if (!auth.currentUser) {
+  const [user, setUser] = useState({ loggedIn: false }); useEffect(() => {
+    const unsubscribe = onAuthStateChange(setUser);
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  if (!user.loggedIn) {
     return (
       <NotFound />
     );
