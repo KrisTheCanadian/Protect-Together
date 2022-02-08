586ed4190f655d8514583a21242ebc5e75d07c9b
gkillick <gkillick@gmail.com>
2022-02-08 01:13:20 -0500

    #57 dashboard & styleguide complete

---
+++ src/components/layout/LayoutContext.tsx
@@ -0,0 +1,12 @@
+import React, {
+  createContext, useContext,
+} from 'react';
+
+export type LayoutContextType = {
+    open: boolean;
+    setOpen: (boolean: boolean) => any;
+}
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+export const LayoutContext = createContext<LayoutContextType>({ open: false, setOpen: (open) => null });
+
+export const useLayoutContext = () => useContext(LayoutContext);
