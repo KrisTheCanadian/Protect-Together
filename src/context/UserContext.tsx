da64de6ae772b0cbe6c6237fe0e2386898e4d21e
Graeme <gkillick@gmail.com>
2022-02-22 19:01:38 -0500

    #17 - refactoring context

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,26 +1,8 @@
-import React, { createContext, useContext } from 'react';
+import { createCtx } from './CreateContext';
 
-// generic createCTx function
-export function createCtx<A>(defaultValue: A) {
-  type UpdateType = React.Dispatch<React.SetStateAction<typeof defaultValue>>;
-  const defaultUpdate: UpdateType = () => defaultValue;
-  const ctx = React.createContext({
-    state: defaultValue,
-    update: defaultUpdate,
-  });
-  function Provider(props: React.PropsWithChildren<Record<string, unknown>>) {
-    const [state, update] = React.useState(defaultValue);
-    const value = React.useMemo(
-      () => ({
-        state,
-        update,
-      }),
-      [state],
-    );
-    return <ctx.Provider value={value} {...props} />;
-  }
-  return [ctx, Provider] as const; // alternatively, [typeof ctx, typeof Provider]
-}
-
-export const [ctx, UserProvider] = createCtx({ firstName: '' });
+export const [ctx, UserProvider] = createCtx({
+  firstName: '',
+  lastName: '',
+  role: '',
+});
 export const UserContext = ctx;
