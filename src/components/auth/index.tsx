0df4ff97252e147c272a69a245ae902716cf06e3
Graeme <gkillick@gmail.com>
2022-02-22 23:25:11 -0500

    #18 fixed eslint rule

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { doc, onSnapshot } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { Navigate, Route, useNavigate } from 'react-router-dom';
