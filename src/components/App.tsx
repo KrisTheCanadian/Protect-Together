c0e6a300c02e87d68df3a3bacb5f912f0a625ccb
Graeme <gkillick@gmail.com>
2022-02-22 19:03:08 -0500

    #49 added logout user story

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,50 +1,55 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React, { useEffect } from 'react';
+
 import '../static/style/App.css';
+
 import { ThemeProvider } from '@mui/material/styles';
 import { Route, Routes } from 'react-router-dom';
+import theme from '../static/style/theme';
 import RegisterPage from '../pages/auth/register';
 import LoginPage from '../pages/auth/login';
-import theme from '../static/style/theme';
 import StyleGuide from './layout/StyleGuide';
 import AuthRequired from './auth';
-import Dashboard from '../pages/auth/dashboard';
+import Dashboard from '../pages/dashboard/dashboard';
 import ChangePassword from '../pages/auth/change';
 import ForgotPassword from '../pages/auth/forgot';
 import Layout from './layout/Layout';
-import { LayoutContext } from '../context/LayoutContext';
-import { createCtx, UserContext, UserProvider } from '../context/UserContext';
 import AdminCreateAccount from '../pages/auth/admincreateaccount';
-import { auth, firestore } from '../config/firebase_config';
+
+import { LayoutContext } from '../context/LayoutContext';
+import { UserContext, UserProvider } from '../context/UserContext';
 
 function App() {
   const { state, update } = React.useContext(UserContext);
+  const [open, setOpen] = React.useState(false);
 
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        <UserProvider>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/forgot" element={<ForgotPassword />} />
-            <Route
-              path="/dashboard"
-              element={<AuthRequired component={<Dashboard />} />}
-            />
-            <Route
-              path="/change"
-              element={<AuthRequired component={<ChangePassword />} />}
-            />
-            <Route path="/styleguide" element={<StyleGuide />} />
-            <Route path="/layout" element={<Layout />} />
-            <Route
-              path="/admincreateaccount"
-              element={<AdminCreateAccount />}
-            />
-          </Routes>
-        </UserProvider>
-      </div>
+      <LayoutContext.Provider value={{ open, setOpen }}>
+        <div>
+          <UserProvider>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/forgot" element={<ForgotPassword />} />
+              <Route
+                path="/dashboard"
+                element={<AuthRequired component={<Dashboard />} />}
+              />
+              <Route
+                path="/change"
+                element={<AuthRequired component={<ChangePassword />} />}
+              />
+              <Route path="/styleguide" element={<StyleGuide />} />
+              <Route path="/layout" element={<Layout />} />
+              <Route
+                path="/admincreateaccount"
+                element={<AdminCreateAccount />}
+              />
+            </Routes>
+          </UserProvider>
+        </div>
+      </LayoutContext.Provider>
     </ThemeProvider>
   );
 }
