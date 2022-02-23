9cb8936f96cc58ef4f55597bbfa88c49325b918f
Graeme <gkillick@gmail.com>
2022-02-22 19:01:38 -0500

    #17 - refactoring context

--- src/context/LayoutContext.tsx
+++ src/context/LayoutContext.tsx
@@ -1,12 +1,13 @@
-import React, {
-  createContext, useContext,
-} from 'react';
+import React, { createContext, useContext } from 'react';
 
 export type LayoutContextType = {
-    open: boolean;
-    setOpen: (boolean: boolean) => any;
-}
+  open: boolean;
+  setOpen: (boolean: boolean) => any;
+};
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-export const LayoutContext = createContext<LayoutContextType>({ open: false, setOpen: (open) => null });
+export const LayoutContext = createContext<LayoutContextType>({
+  open: false,
+  setOpen: (open) => null,
+});
 
 export const useLayoutContext = () => useContext(LayoutContext);
