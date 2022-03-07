8728ccb3667ec106e0d43bb9a0ce91ae5f18f965
Kim Dera <39693234+kimdera@users.noreply.github.com>
2022-03-07 18:15:23 -0500

    Patient test result #34 (#107)
    * Added covid test form to Patient dashboard (#34)
    
    * changed form format
    
    * fixed form format and button placement
    
    * Added test functionalities #34
    
    * Added test date to covid test form
    
    * Added test result field to the db #34
    
    * fixing package-lock
    
    * fixed last line of package-lock
    
    * Added test form verification #34
    
    * Changing directory for test form #34
    
    * merging test changes #34
    
    * fixed the submit verification #34
    
    * added breakpoint for small screens
    
    * fixed submit verification #34
    
    * fixed submit verification #34
    
    * #107 fix to allow scrolling
    
    Co-authored-by: Sami <smerhi.sm@gmail.com>
    Co-authored-by: Graeme <gkillick@gmail.com>

--- src/components/dashboard/PatientDashboard.tsx
+++ src/components/dashboard/PatientDashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MailIcon from '@mui/icons-material/Mail';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
@@ -19,24 +19,32 @@ import Header from '../layout/Header';
 import MainContent from '../layout/MainContent';
 import SideBar from '../layout/SideBar';
 import { UserContext } from '../../context/UserContext';
-import AdminCreateAccount from '../../pages/auth/admincreateaccount';
+import UpdateTestResult from './patienttestresult';
+import theme from '../../static/style/theme';
 
 const style = {
   position: 'absolute' as const,
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: '50%',
+  width: 600,
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    padding: 0,
+  },
   boxShadow: 0,
   margin: 0,
   p: 4,
 };
 
 function PatientDashboard() {
-  const [modalOpen, setModalOpen] = React.useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   const handleOpenQuiz = () => setModalOpen(true);
   const handleClose = () => setModalOpen(false);
   const navigate = useNavigate();
+  const [testOpen, setTestOpen] = useState(false);
+  const handleTestOpen = () => setTestOpen(true);
+  const handleTestClose = () => setTestOpen(false);
 
   const { state, update } = React.useContext(UserContext);
 
@@ -44,6 +52,9 @@ function PatientDashboard() {
     <Box sx={{ display: 'flex', width: '100%' }}>
       <CssBaseline />
       <Header title={`Welcome ${state.firstName}`} subtitle="Stay safe">
+        <Button variant="contained" color="info" sx={{ mr: 1 }} onClick={handleTestOpen}>
+          Add Covid-19 Test
+        </Button>
         <Button variant="contained" color="primary" onClick={() => { navigate('/symptomsForm'); }}>
           Ask for Help
         </Button>
@@ -63,6 +74,16 @@ function PatientDashboard() {
         <Typography paragraph>{state.firstName}</Typography>
       </MainContent>
 
+      <Modal
+        open={testOpen}
+        onClose={handleTestClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <UpdateTestResult handleTestClose={handleTestClose} />
+        </Box>
+      </Modal>
       <Modal
         open={modalOpen}
         onClose={handleClose}
---
+++ src/components/dashboard/patienttestresult.tsx
@@ -0,0 +1,280 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  Paper,
+  Radio,
+  RadioGroup,
+  Typography,
+  TextField,
+} from '@mui/material';
+import useId from '@mui/material/utils/useId';
+import { arrayUnion, doc, setDoc, Timestamp } from 'firebase/firestore';
+import { DatePicker, LocalizationProvider } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { auth, firestore } from '../../config/firebase_config';
+
+type Props = {
+  handleTestClose: any;
+};
+function UpdateTestResult({ handleTestClose }: Props) {
+  const [error, setError] = useState<string>('');
+  const [testDate, setTestDate] = useState<Date | null>(null);
+  const [testType, setTestType] = useState<string>('');
+  const [testResult, setTestResult] = useState<string>('');
+  const [status, setStatus] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (testResult === '' || !testDate || testType === '') {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
+  }, [testDate, testType, testResult]);
+
+  const addPatientTestResults = () => {
+    const uid = auth.currentUser?.uid;
+    const user = firestore.collection('users').doc(uid);
+    user.update({
+      testsResults: arrayUnion({
+        testResult,
+        testType,
+        testDate,
+      }),
+    });
+  };
+
+  return (
+    <Grid
+      container
+      xs={12}
+      sx={{
+        bgcolor: 'primary.contrastText',
+        borderRadius: 2,
+        boxShadow: 6,
+        padding: 6,
+        flexDirection: 'column',
+        width: '100%',
+        maxHeight: '100vh',
+        overflow: 'scroll',
+      }}
+    >
+      <Grid
+        container
+        item
+        xs={12}
+        rowSpacing={1.5}
+        sx={{
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          sx={{
+            width: '75%',
+          }}
+        >
+          <Paper
+            sx={{
+              borderRadius: 4,
+              bgcolor: 'secondary.main',
+              p: 4,
+            }}
+          >
+            <FormLabel
+              sx={{
+                fontSize: 20,
+                mb: 2,
+              }}
+            >
+              When did you take your test?
+            </FormLabel>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Select Test Date"
+                value={testDate}
+                onChange={(newValue) => {
+                  setTestDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} sx={{ mr: 2 }} required />}
+              />
+            </LocalizationProvider>
+          </Paper>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            width: '75%',
+          }}
+        >
+          <Paper
+            sx={{
+              borderRadius: 4,
+              bgcolor: 'secondary.main',
+              p: 4,
+            }}
+          >
+            <FormControl>
+              <FormLabel
+                sx={{
+                  fontSize: 20,
+                }}
+                focused={false}
+              >
+                Which test did you take?
+              </FormLabel>
+              <RadioGroup
+                value={testType}
+                onChange={(event) => setTestType(event.target.value)}
+              >
+                <FormControlLabel
+                  value="PCRTest"
+                  control={<Radio />}
+                  label="PCR Test"
+                  sx={{
+                    borderRadius: 4,
+                    bgcolor: 'primary.contrastText',
+                    mb: 2,
+                    mt: 2,
+                  }}
+                />
+                <FormControlLabel
+                  value="rapidAntigenTest"
+                  control={<Radio />}
+                  label="Rapid Antigen Test"
+                  sx={{
+                    borderRadius: 4,
+                    bgcolor: 'primary.contrastText',
+                    mb: 2,
+                    pr: 1,
+                  }}
+                />
+                <FormControlLabel
+                  value="antibodyTest"
+                  control={<Radio />}
+                  label="Antibody Test"
+                  sx={{
+                    borderRadius: 4,
+                    bgcolor: 'primary.contrastText',
+                  }}
+                />
+              </RadioGroup>
+            </FormControl>
+          </Paper>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            width: '75%',
+          }}
+        >
+          <Paper
+            sx={{
+              borderRadius: 4,
+              bgcolor: 'secondary.main',
+              p: 4,
+              mb: 2,
+            }}
+          >
+            <FormLabel
+              sx={{
+                fontSize: 20,
+              }}
+            >
+              What was your test result?
+            </FormLabel>
+            <FormControl>
+              <RadioGroup
+                value={testResult}
+                onChange={(event) => setTestResult(event.target.value)}
+              >
+                <FormControlLabel
+                  value="positive"
+                  control={<Radio />}
+                  label="Covid-19 Positive"
+                  sx={{
+                    borderRadius: 4,
+                    bgcolor: 'primary.contrastText',
+                    mb: 2,
+                    mt: 2,
+                  }}
+                />
+                <FormControlLabel
+                  value="negative"
+                  control={<Radio />}
+                  label="Covid-19 Negative"
+                  sx={{
+                    borderRadius: 4,
+                    bgcolor: 'primary.contrastText',
+                    pr: 1,
+                  }}
+                />
+              </RadioGroup>
+            </FormControl>
+          </Paper>
+          <Grid
+            container
+            item
+            spacing={2}
+            sx={{
+              flexDirection: 'row',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Grid item xs={4}>
+              <Paper
+                sx={{
+                  mt: 2,
+                }}
+              >
+                <Button
+                  type="button"
+                  onClick={() => {
+                    handleTestClose();
+                  }}
+                  color="warning"
+                  fullWidth
+                  variant="contained"
+                >
+                  Cancel
+                </Button>
+              </Paper>
+            </Grid>
+            <Grid item xs={4}>
+              <Paper
+                sx={{
+                  mt: 2,
+                }}
+              >
+                <Button
+                  type="button"
+                  onClick={() => {
+                    handleTestClose();
+                    addPatientTestResults();
+                  }}
+                  fullWidth
+                  variant="contained"
+                  disabled={status}
+                >
+                  Submit
+                </Button>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+    </Grid>
+  );
+}
+
+export default UpdateTestResult;
