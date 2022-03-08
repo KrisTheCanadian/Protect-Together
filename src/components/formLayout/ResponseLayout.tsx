35cd28e64b3b4096e4f547b041eb44f4f6c1e640
Mimi <m_bogdev@live.concordia.ca>
2022-03-07 23:40:15 -0500

    #100  Added Button to response + Code Refactoring

--- src/components/formLayout/ResponseLayout.tsx
+++ src/components/formLayout/ResponseLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Box, CardMedia } from '@mui/material';
+import { Container, Typography, Box, CardMedia, Button } from '@mui/material';
 import Image0 from '../../static/style/images/Hospital.png';
 import Image1 from '../../static/style/images/Symptoms.png';
 import Image2 from '../../static/style/images/Doctor.png';
@@ -32,7 +32,7 @@ export default function ResponseLayout({ selection }: any) {
         >
           <CardMedia sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             {selection === 0 && <img src={Image0} style={{ maxWidth: '50%' }} alt="Emergency Room" />}
-            {selection === 1 && <img src={Image1} style={{ maxWidth: '40%' }} alt="Symptoms" />}
+            {selection === 1 && <img src={Image1} style={{ maxWidth: '60%' }} alt="Symptoms" />}
             {selection === 2 && <img src={Image2} style={{ maxWidth: '40%' }} alt="Doctor" />}
           </CardMedia>
           <Container>
@@ -52,13 +52,48 @@ export default function ResponseLayout({ selection }: any) {
                 marginTop: 1,
                 flexGrow: 1,
                 textAlign: 'center',
+                marginBottom: 2,
               }}
             >
               {data[selection]?.subTitle}
             </Typography>
-          </Container>
 
+          </Container>
+          <Container
+            sx={{
+              position: 'fixed',
+              bottom: 10,
+              width: '100%',
+              height: 60,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingTop: 4,
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                textAlign: 'center',
+                color: '#383737',
+                marginRight: '1rem',
+              }}
+            >
+              Would you like to be contacted by one of our doctors?
+            </Typography>
+            <Button
+              variant="text"
+            >
+              Yes
+            </Button>
+            <Button
+              variant="text"
+            >
+              No
+            </Button>
+          </Container>
         </Container>
+
       </Box>
     </div>
   );
