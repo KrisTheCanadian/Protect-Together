84fcf5d5c27e951522dd8b9647058b69c780caac
KD <kim199@yahoo.com>
2022-03-11 21:54:31 -0500

    added the covid-19 map #36

--- src/components/dashboard/PatientDashboard.tsx
+++ src/components/dashboard/PatientDashboard.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import MailIcon from '@mui/icons-material/Mail';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+// import Card from '@material-ui/core/Card';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardMedia from '@material-ui/core/CardMedia';
 import {
   Button,
   Box,
@@ -14,13 +17,18 @@ import {
   Typography,
   Modal,
 } from '@mui/material';
+import Iframe from 'react-iframe';
 import { useNavigate } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import Header from '../layout/Header';
 import MainContent from '../layout/MainContent';
 import SideBar from '../layout/SideBar';
 import { UserContext } from '../../context/UserContext';
 import UpdateTestResult from './patienttestresult';
 import theme from '../../static/style/theme';
+// import Chart from './Chart';
+// import Deposits from './Deposits.tsx';
 
 const style = {
   position: 'absolute' as const,
@@ -72,6 +80,61 @@ function PatientDashboard() {
       </SideBar>
       <MainContent>
         <Typography paragraph>{state.firstName}</Typography>
+        <Box m={2} pt={3} />
+        <Typography
+          variant="h4"
+          sx={{
+            color: 'rgba(0, 0, 0, 0.87)',
+            paddingTop: 1,
+          }}
+        >
+          Covid-19 News
+
+          <Typography sx={{ fontSize: '1rem', color: 'rgba(0, 0, 0, 0.87)' }}>
+            Do not forget to follow the safety policies and stay safe!
+          </Typography>
+          <Typography sx={{ fontSize: '1rem', color: 'rgba(0, 0, 0, 0.87)' }}>
+            Cumulative Covid-19 Statics for Canada
+          </Typography>
+        </Typography>
+        <Box m={1} pt={1} />
+        <Grid container spacing={3}>
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 390,
+              }}
+            >
+              {/* <Chart /> */}
+              <Iframe
+                url="https://covid19canada.maps.arcgis.com/apps/Minimalist/index.html?appid=b3baccb0f30e4516b8e64009b3383f55"
+                position="absolute"
+                width="60%"
+                id="myId"
+                className="myClassname"
+                height="40%"
+                styles={{ height: '25px' }}
+              />
+            </Paper>
+          </Grid>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 390,
+              }}
+            >
+              {/* <Deposits /> */}
+            </Paper>
+          </Grid>
+        </Grid>
       </MainContent>
 
       <Modal
