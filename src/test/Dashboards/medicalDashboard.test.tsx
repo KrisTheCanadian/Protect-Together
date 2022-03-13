a928b52694c8a76fb4b3735aa747da9259a943bb
Fadi Albasha <fadi.albasha@gmail.com>
2022-03-13 00:29:05 -0500

    #44 Refactored Medical dashboard

--- src/test/Dashboards/medicalDashboard.test.tsx
+++ src/test/Dashboards/medicalDashboard.test.tsx
@@ -4,7 +4,7 @@
 import { cleanup, render } from '@testing-library/react';
 import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import MedicalDashboard from '../../components/dashboard/MedicalDashboard';
+import MedicalDashboard from '../../components/dashboard/MedicalView';
 import { UserProvider } from '../../context/UserContext';
 
 jest.mock('firebase/compat/app', () => {
