ac04638a9f2a60f2f1ad195a85f60a04e784fca0
KD <kim199@yahoo.com>
2022-03-14 16:17:55 -0400

    fixed pr reviews #36

--- src/components/dashboard/PatientDashboard.tsx
+++ src/components/dashboard/PatientDashboard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 import React, { useEffect, useState } from 'react';
 import MailIcon from '@mui/icons-material/Mail';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
