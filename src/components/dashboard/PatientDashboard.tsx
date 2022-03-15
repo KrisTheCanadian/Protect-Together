cb70225e3dbaced08bb2ce81c60a15cbca89c9f3
Kim Dera <39693234+kimdera@users.noreply.github.com>
2022-03-14 21:07:45 -0400

    #36 Adding Covid stats + safety info + fetching data from API (#115)
    * added the covid-19 map #36
    
    * #36 added country covid data search + api
    
    * #36 fixed responsiveness + added covid safety info
    
    * fixed pr reviews #36
    
    * fixed pr reviews #36

--- src/components/dashboard/PatientDashboard.tsx
+++ src/components/dashboard/PatientDashboard.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-shadow */
+import React, { useEffect, useState } from 'react';
 import MailIcon from '@mui/icons-material/Mail';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+// eslint-disable-next-line import/no-unresolved
+import '../../static/style/CovidData.css';
 import {
   Button,
   Box,
@@ -11,10 +14,16 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  ListItemAvatar,
   Typography,
   Modal,
 } from '@mui/material';
+import FolderIcon from '@mui/icons-material/Folder';
+import Avatar from '@mui/material/Avatar';
+import Iframe from 'react-iframe';
 import { useNavigate } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import Header from '../layout/Header';
 import MainContent from '../layout/MainContent';
 import SideBar from '../layout/SideBar';
@@ -45,8 +54,54 @@ function PatientDashboard() {
   const [testOpen, setTestOpen] = useState(false);
   const handleTestOpen = () => setTestOpen(true);
   const handleTestClose = () => setTestOpen(false);
-
   const { state, update } = React.useContext(UserContext);
+
+  const [country, setCountry] = useState('');
+  const [cases, setCases] = useState('');
+  const [recovered, setRecovered] = useState('');
+  const [deaths, setDeaths] = useState('');
+  const [todayCases, setTodayCases] = useState('');
+  const [deathCases, setDeathCases] = useState('');
+  const [recoveredCases, setRecoveredCases] = useState('');
+  const [userInput, setUserInput] = useState('');
+
+  const setData = ({
+    country,
+    cases,
+    deaths,
+    recovered,
+    todayCases,
+    todayDeaths,
+    todayRecovered,
+  }: any) => {
+    setCountry(country);
+    setCases(cases);
+    setRecovered(recovered);
+    setDeaths(deaths);
+    setTodayCases(todayCases);
+    setDeathCases(todayDeaths);
+    setRecoveredCases(todayRecovered);
+  };
+
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/countries')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  const handleSearch = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setUserInput(e.target.value);
+  };
+  const handleSubmit = (props: { preventDefault: () => void; }) => {
+    props.preventDefault();
+    fetch(`https://disease.sh/v3/covid-19/countries/${userInput}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  };
 
   return (
     <Box sx={{ display: 'flex', width: '100%' }}>
@@ -71,7 +126,200 @@ function PatientDashboard() {
         <Divider />
       </SideBar>
       <MainContent>
-        <Typography paragraph>{state.firstName}</Typography>
+        <Typography
+          variant="h4"
+          sx={{
+            color: 'rgba(0, 0, 0, 0.87)',
+            paddingTop: 1,
+          }}
+        >
+          Covid-19 Statistics
+          <Typography sx={{ fontSize: '1rem', color: 'rgba(0, 0, 0, 0.87)' }}>
+            Do not forget to follow the safety policies and stay safe!
+          </Typography>
+        </Typography>
+        <Box m={1} pt={1} />
+        <Grid container spacing={3}>
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 390,
+              }}
+            >
+              {/* <Chart /> */}
+              <Iframe
+                // eslint-disable-next-line max-len
+                url="https://covid19canada.maps.arcgis.com/apps/Minimalist/index.html?appid=b3baccb0f30e4516b8e64009b3383f55"
+                height="100%"
+              />
+            </Paper>
+          </Grid>
+          {/* box 1 */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 390,
+              }}
+            >
+
+              <div className="covidData">
+                <h4> COVID-19 Statistics Per Country</h4>
+                <div className="covidData__input">
+                  <form onSubmit={handleSubmit}>
+                    {/* input county name */}
+                    <input onChange={handleSearch} placeholder="Enter Country Name" />
+                    <br />
+                    <button color="secondary" type="submit">Search</button>
+                  </form>
+                </div>
+
+                {/* Showing the details of the country */}
+                <div className="covidData__country__info">
+                  <p>
+                    Country Name :
+                    {' '}
+                    {country}
+                    {' '}
+                  </p>
+
+                  <p>
+                    Cases :
+                    {' '}
+                    {cases}
+                  </p>
+
+                  <p>
+                    Deaths :
+                    {' '}
+                    {deaths}
+                  </p>
+
+                  <p>
+                    Recovered :
+                    {' '}
+                    {recovered}
+                  </p>
+
+                  <p>
+                    Cases Today :
+                    {' '}
+                    {todayCases}
+                  </p>
+
+                  <p>
+                    Deaths Today :
+                    {' '}
+                    {deathCases}
+                  </p>
+
+                  <p>
+                    Recovered Today :
+                    {' '}
+                    {recoveredCases}
+                  </p>
+                </div>
+              </div>
+              {/* <box 2 /> */}
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+              Basic Protective Measures Against Coronavirus
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="1." src="../../static/style/images/1.png" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Clean your hands "
+              secondary={(
+                <>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  />
+                  Wash hands often with soap and water for at least 20s.
+                </>
+          )}
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="2." src="/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Wear a facemask"
+              secondary={(
+                <>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  />
+                  You should wear facemask when you are around other poeple.
+                </>
+          )}
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="3." src="/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Avoid touching your face"
+              secondary={(
+                <>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  />
+                  Hands touch many surfaces and can pick up viruses.
+                </>
+          )}
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="4." src="../../static/style/images/2.png" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Avoid close contact"
+              secondary={(
+                <>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  />
+                  Put distance between yourself and other people.
+                </>
+          )}
+            />
+          </ListItem>
+        </List>
+
       </MainContent>
 
       <Modal
