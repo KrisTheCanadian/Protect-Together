02b134c5665ba9ce1c1f854adfdcfa9d90a32a81
Kris <kristhecanadian101@gmail.com>
2022-03-25 15:49:18 -0400

    oops

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -6,6 +6,7 @@ import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../config/firebase_config';
 import { useLayoutContext } from '../../context/LayoutContext';
+import { UserContext } from '../../context/UserContext';
 
 const drawerWidth = 240;
 type Props = {
