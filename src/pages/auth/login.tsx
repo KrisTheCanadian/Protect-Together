662ee0f921f067084bbe0bd8bc81713983e0b5ee
Sami <smerhi.sm@gmail.com>
2022-04-06 01:41:40 -0400

    #98 login image on top on mobile

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -36,6 +36,89 @@ export default function LoginPage() {
     }
   });
 
+  if (phoneSize) {
+    return (
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <PanelSwapper showPanel="flex" panelSmallSize={6} panelMedSize={7} />
+        <Grid
+          item
+          container
+          xs={12}
+          sm={6}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ direction: phoneSize ? 'column' : 'row ' }}
+        >
+          <Box
+            sx={{
+              my: 4,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h4">
+              Welcome
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                inputProps={{ 'data-testid': 'email' }}
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={(event) => setEmail(event.target.value)}
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                inputProps={{ 'data-testid': 'password' }}
+                autoComplete="current-password"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <Button
+                type="button"
+                onClick={() => LoginWithEmailAndPassword()}
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="/forgot" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/register" variant="body2">
+                    Dont have an account? Sign Up
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+            {error && <Alert severity="error">{error}</Alert>}
+          </Box>
+        </Grid>
+      </Grid>
+    );
+  }
   return (
     <Grid container component="main" sx={{ height: '100vh' }}>
       <Grid
