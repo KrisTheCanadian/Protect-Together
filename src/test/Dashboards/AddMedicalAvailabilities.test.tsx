54c0270e5097c5e1a925a0cb0bc0729939814ff9
Kim Dera <39693234+kimdera@users.noreply.github.com>
2022-04-05 22:48:52 -0400

    DevOps #130 - Added Jest Tests (#172)
    * #130 added a test and the view all option to tests
    
    * #130 Added test for medical availabilites
    
    * #130 Added test for SymptomsUpdate
    
    * #130 added tests for symptoms questions
    
    * #130 fixed file name
    
    * #130 fixed package.json
    
    * #130 fixed a test
    
    * #130 Deleted empty lines from test file

---
+++ src/test/Dashboards/AddMedicalAvailabilities.test.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-promise-executor-return */
+/* eslint-disable react/react-in-jsx-scope */
+import {
+  cleanup, fireEvent, queryByAttribute, render,
+} from '@testing-library/react';
+import { Firestore } from 'firebase/firestore';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import AddMedicalAvailabilities from '../../components/dashboard/AdminDashboard/AdminTable/AddMedicalAvailabilities';
+// eslint-disable-next-line import/no-extraneous-dependencies
+
+jest.mock('firebase/compat/app', () => {
+  // mock Firestore
+  const Firestore = () => ({
+    collection: () => ({
+      where: () => ({
+        onSnapshot: () => null,
+        where: () => ({
+          onSnapshot: () => null,
+        }),
+      }),
+      doc: () => ({
+        get: () => ({
+          then: () => null,
+        }),
+      }),
+    }),
+    // to mock attributes
+    FieldValue: {
+      serverTimestamp: jest.fn(),
+    },
+
+  });
+  const firestore = Firestore;
+  const app = jest.requireActual('firebase/compat/app');
+  const auth = () => ({
+    onAuthStateChanged: () => {
+      jest.fn();
+    },
+  });
+  auth.GoogleAuthProvider = jest.fn();
+  auth.signInWithEmailAndPassword = jest.fn();
+  return {
+    __esModule: true,
+    ...app,
+    default: {
+      auth,
+      initializeApp: jest.fn(),
+      firestore,
+    },
+  };
+});
+
+const getById = queryByAttribute.bind(null, 'id');
+jest.mock('firebase/firestore');
+afterEach(cleanup);
+test('Medical Availability renders correctly', async () => {
+  const component = render(<BrowserRouter><AddMedicalAvailabilities handleTestRClose={undefined} /></BrowserRouter>);
+  const avai = component.getAllByText('Save');
+  expect(avai).toBeTruthy();
+  const Appointments = component.getByText('Appointment Slots');
+  expect(Appointments).toBeTruthy();
+});
